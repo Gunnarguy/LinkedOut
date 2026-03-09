b7a23b4ecd54995494a98c19b04fb9d06faca400
gunnarguy <gunnarguy@me.com>
2026-03-09 10:42:26 -0700

    Initial Commit

---
+++ LinkedOut/LinkedOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedOutApp.swift
+//  LinkedOut
+//
+//  Created by Gunnar Hostetler on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct LinkedOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
